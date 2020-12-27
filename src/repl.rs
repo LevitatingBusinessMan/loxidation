eaa25aab43422b170c6767cf52a1682e4670310d
Rein Fernhout <public@reinfernhout.xyz>
2020-12-27 04:41:05 +0100

    Added prompt to repl

--- src/repl.rs
+++ src/repl.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::io::prelude::Write;
 use crate::compiler::compile;
 use crate::vm;
 
@@ -6,6 +7,8 @@ pub fn repl() {
 	let mut buf = String::new();
 	let stdin = io::stdin();
 	loop {
+		print!("lox> ");
+		io::stdout().flush().unwrap();
 		stdin.read_line(&mut buf).unwrap();
 		let chunk = compile(buf).unwrap();
 		println!("{}", chunk.disassemble("REPL"));
